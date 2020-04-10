8b4f6739fbf3f26bc8cbb96b2f6499e8bf042c07
Amit Saran <amitsaran@gmail.com>
2020-04-10 15:43:48 -0400

    minor changes

--- cppBSTree/BSTree.cpp
+++ cppBSTree/BSTree.cpp
@@ -29,8 +29,7 @@ void BSTree::add(const int data) {
 	Node* n = new Node(data);
 	if (root == NULL) {		
 		this->_setRoot(n);
-	}
-	else {
+	} else {
 		this->_add(n, root);
 	}
 }
@@ -39,8 +38,7 @@ void BSTree::remove(const int data) {
 	Node* location = NULL;
 	Node* parent = NULL;
 	this->find(data, location, parent);
-	this->_remove(location, parent);
-	
+	this->_remove(location, parent);	
 }
 
 void BSTree::find(const int data, Node*& location, Node*& parent)
@@ -81,32 +79,32 @@ int BSTree::height() {
 std::vector<Node*> BSTree::traverse(const TraverseType traverseType) {
 	std::vector<Node*> nodesList;
 	Node* root = this->root();
-	if (root) {
-		int type = (int)traverseType;
-		switch (type) {
-			case (TraverseType::DFSInOrder):
-			{
-				this->_traverse_DFSInOrder(root, nodesList);
-				break;
-			}
-			case(TraverseType::DFSPreOrder):
-			{
-				this->_traverse_DFSPreOrder(root, nodesList);
-				break;
-			}
-			case(TraverseType::DFSPostOrder):
-			{
-				this->_traverse_DFSPostOrder(root, nodesList);
-				break;
-			}
-			case(TraverseType::BFS):
-			{
-				this->_traverse_BFS(root, nodesList);
-				break;
-			}
+	if (root == NULL) {
+		return nodesList;
+	}
+	int type = (int)traverseType;
+	switch (type) {
+		case (TraverseType::DFSInOrder):
+		{
+			this->_traverse_DFSInOrder(root, nodesList);
+			break;
+		}
+		case(TraverseType::DFSPreOrder):
+		{
+			this->_traverse_DFSPreOrder(root, nodesList);
+			break;
+		}
+		case(TraverseType::DFSPostOrder):
+		{
+			this->_traverse_DFSPostOrder(root, nodesList);
+			break;
+		}
+		case(TraverseType::BFS):
+		{
+			this->_traverse_BFS(root, nodesList);
+			break;
 		}
 	}
-
 	return nodesList;
 }
 
@@ -116,124 +114,124 @@ void BSTree::_setRoot(Node* root) {
 
 // private functions
 void BSTree::_add(Node*& nodeToBeInstrted, Node*& atNode) {
-	if ((nodeToBeInstrted != NULL) && (atNode != NULL)) {
-		if (atNode->getData() > nodeToBeInstrted->getData()) {
-			Node* left = atNode->getLeft();
-			if (left != NULL) {
-				this->_add(nodeToBeInstrted, left);
-			}
-			else {
-				atNode->setLeft(nodeToBeInstrted);
-			}
+	if ((nodeToBeInstrted == NULL) || (atNode == NULL)) {
+		return;
+	}
+	if (atNode->getData() > nodeToBeInstrted->getData()) {
+		Node* left = atNode->getLeft();
+		if (left != NULL) {
+			this->_add(nodeToBeInstrted, left);
 		}
 		else {
-			Node* right = atNode->getRight();
-			if (right != NULL) {
-				this->_add(nodeToBeInstrted, right);
-			}
-			else {
-				atNode->setRight(nodeToBeInstrted);
-			}
+			atNode->setLeft(nodeToBeInstrted);
+		}
+	} else {
+		Node* right = atNode->getRight();
+		if (right != NULL) {
+			this->_add(nodeToBeInstrted, right);
+		}
+		else {
+			atNode->setRight(nodeToBeInstrted);
 		}
 	}
 }
 
 void  BSTree::_find(const int data, Node* startNode, Node* parentOfStartNode, Node*& location, Node*& parent) {
-	if (startNode != NULL) {
-		int _data = startNode->getData();
-		if (_data == data) {
-			location = startNode;
-			parent = parentOfStartNode;
+	if (startNode == NULL) {
+		return;
+	}
+	int _data = startNode->getData();
+	if (_data == data) {
+		location = startNode;
+		parent = parentOfStartNode;
+	} else if (_data > data) {
+		Node* left = startNode->getLeft();
+		if (left) {
+			this->_find(data, left, startNode, location, parent);
 		}
-		else if (_data > data) {
-			Node* left = startNode->getLeft();
-			if (left) {
-				this->_find(data, left, startNode, location, parent);
-			}
-		}
-		else {
-			Node* right = startNode->getRight();
-			if (right) {
-				this->_find(data, right, startNode, location, parent);
-			}
+	} else {
+		Node* right = startNode->getRight();
+		if (right) {
+			this->_find(data, right, startNode, location, parent);
 		}
 	}
 }
 
 void BSTree::_remove(Node*& location, Node*& parent) {
-	if (location) {
-		// Case 1:  No child
-		Node* left = location->getLeft();
-		Node* right = location->getRight();
-		if (left == NULL && right == NULL) {
-			if (parent->getLeft() == location) {
-				parent->setLeft(NULL);
-			}
-			if (parent->getRight() == location) {
-				parent->setRight(NULL);
-			}
-			delete location;
-			location = NULL;
+	if (location == NULL) {
+		return;
+	}	
+	Node* left = location->getLeft();
+	Node* right = location->getRight();
+	// Case 1:  No child
+	if (left == NULL && right == NULL) {
+		if (parent->getLeft() == location) {
+			parent->setLeft(NULL);
 		}
-		//Case 2a: One child - left is null
-		else if (left == NULL) {
-			if (parent->getLeft() == location) {
-				parent->setLeft(location->getRight());
-			}
-			if (parent->getRight() == location) {
-				parent->setRight(location->getRight());
-			}
-			location->setRight(NULL);
-			delete location;
-			location = NULL;
+		if (parent->getRight() == location) {
+			parent->setRight(NULL);
 		}
-		//Case 2b: One child - right is null
-		else if (right == NULL) {
-			if (parent->getLeft() == location) {
-				parent->setLeft(location->getLeft());
-			}
-			if (parent->getRight() == location) {
-				parent->setRight(location->getLeft());
-			}
-			location->setLeft(NULL);
-			delete location;
-			location = NULL;
+		delete location;
+		location = NULL;
+	}
+	//Case 2a: One child - left is null
+	else if (left == NULL) {
+		if (parent->getLeft() == location) {
+			parent->setLeft(location->getRight());
 		}
-		// case 3: Two children
-		else {
-			Node* smallestInRightTree = NULL;
-			Node* parentOfSmallest = NULL;
-			// find the smallest in the right side of the tree
-			this->_findSmallest(location->getRight(), parent, smallestInRightTree, parentOfSmallest);
-			location->setData(smallestInRightTree->getData());
-			this->_remove(smallestInRightTree, parentOfSmallest);
+		if (parent->getRight() == location) {
+			parent->setRight(location->getRight());
 		}
+		location->setRight(NULL);
+		delete location;
+		location = NULL;
+	}
+	//Case 2b: One child - right is null
+	else if (right == NULL) {
+		if (parent->getLeft() == location) {
+			parent->setLeft(location->getLeft());
+		}
+		if (parent->getRight() == location) {
+			parent->setRight(location->getLeft());
+		}
+		location->setLeft(NULL);
+		delete location;
+		location = NULL;
+	}
+	// case 3: Two children
+	else {
+		Node* smallestInRightTree = NULL;
+		Node* parentOfSmallest = NULL;
+		// find the smallest in the right side of the tree
+		this->_findSmallest(location->getRight(), parent, smallestInRightTree, parentOfSmallest);
+		location->setData(smallestInRightTree->getData());
+		this->_remove(smallestInRightTree, parentOfSmallest);
 	}
 }
 
 void  BSTree::_findSmallest(Node* startNode, Node* startParentNode, Node*& location, Node*& parent) {
-	if (startNode != NULL) {
-		Node* left = startNode->getLeft();
-		if (left == NULL) {
-			location = startNode;
-			parent = startParentNode;
-		}
-		else {
-			_findSmallest(left, startNode, location, parent);
-		}
+	if (startNode == NULL) {
+		return;
+	}
+	Node* left = startNode->getLeft();
+	if (left == NULL) {
+		location = startNode;
+		parent = startParentNode;
+	} else {
+		_findSmallest(left, startNode, location, parent);
 	}
 }
 
 void  BSTree::_findLargest(Node* startNode, Node* startParentNode, Node*& location, Node*& parent) {
-	if (startNode != NULL) {
-		Node* right = startNode->getRight();
-		if (right == NULL) {
-			location = startNode;
-			parent = startParentNode;
-		}
-		else {
-			_findLargest(right, startNode, location, parent);
-		}
+	if (startNode == NULL) {
+		return;
+	}
+	Node* right = startNode->getRight();
+	if (right == NULL) {
+		location = startNode;
+		parent = startParentNode;
+	} else {
+		_findLargest(right, startNode, location, parent);
 	}
 }
 
@@ -258,65 +256,69 @@ int BSTree::_height(Node* node) {
 
 // DFSInOrder: LNR(Left-Node-Right)
 void BSTree::_traverse_DFSInOrder(Node* node, std::vector<Node*>& nodesList) {	
-	if (node) {
-		Node* left = node->getLeft();
-		Node* right = node->getRight();
-		if (left) {
-			this->_traverse_DFSInOrder(left, nodesList);
-		}
-		nodesList.push_back(node);
-		if(right){
-			this->_traverse_DFSInOrder(right, nodesList);
-		}
+	if (node == NULL) {
+		return;
+	}
+	Node* left = node->getLeft();
+	Node* right = node->getRight();
+	if (left) {
+		this->_traverse_DFSInOrder(left, nodesList);
+	}
+	nodesList.push_back(node);
+	if(right){
+		this->_traverse_DFSInOrder(right, nodesList);
 	}
 }
 
 // DFSPreOrder: NLR(Node-Left-Right)
 void BSTree::_traverse_DFSPreOrder(Node* node, std::vector<Node*>& nodesList) {
-	if (node) {
-		Node* left = node->getLeft();
-		Node* right = node->getRight();
-		nodesList.push_back(node);
-		if (left) {
-			this->_traverse_DFSPreOrder(left, nodesList);
-		}
-		if (right) {
-			this->_traverse_DFSPreOrder(right, nodesList);
-		}
+	if (node == NULL) {
+		return;
+	}
+	Node* left = node->getLeft();
+	Node* right = node->getRight();
+	nodesList.push_back(node);
+	if (left) {
+		this->_traverse_DFSPreOrder(left, nodesList);
+	}
+	if (right) {
+		this->_traverse_DFSPreOrder(right, nodesList);
 	}
 }
 
 // DFSPostOrder: LRN(Left-Right-Node)
 void BSTree::_traverse_DFSPostOrder(Node* node, std::vector<Node*>& nodesList) {
-	if (node) {
-		Node* left = node->getLeft();
-		Node* right = node->getRight();
-		if (left) {
-			this->_traverse_DFSPostOrder(left, nodesList);
-		}
-		if (right) {
-			this->_traverse_DFSPostOrder(right, nodesList);
-		}
-		nodesList.push_back(node);		
+	if (node == NULL) {
+		return;
 	}
+	Node* left = node->getLeft();
+	Node* right = node->getRight();
+	if (left) {
+		this->_traverse_DFSPostOrder(left, nodesList);
+	}
+	if (right) {
+		this->_traverse_DFSPostOrder(right, nodesList);
+	}
+	nodesList.push_back(node);
 }
 
 void BSTree::_traverse_BFS(Node* node, std::vector<Node*>& nodesList) {
 	std::queue<Node*> tempQ;
-	if (node) {
-		tempQ.push(node);
-		while (!(tempQ.empty())) {			
-			Node* tempNode = tempQ.front();
-			nodesList.push_back(tempNode);
-			Node* left = tempNode->getLeft();
-			Node* right = tempNode->getRight();
-			if (left) {
-				tempQ.push(left);
-			}
-			if (right) {
-				tempQ.push(right);
-			}
-			tempQ.pop();
+	if (node = NULL) {
+		return;
+	}
+	tempQ.push(node);
+	while (!(tempQ.empty())) {			
+		Node* tempNode = tempQ.front();
+		nodesList.push_back(tempNode);
+		Node* left = tempNode->getLeft();
+		Node* right = tempNode->getRight();
+		if (left) {
+			tempQ.push(left);
 		}
+		if (right) {
+			tempQ.push(right);
+		}
+		tempQ.pop();
 	}
 }
